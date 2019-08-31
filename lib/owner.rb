a4b3300aa84b6c4ec020631b28773c1047724c6b
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-31 16:02:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,9 +2,12 @@ class Owner
   
   attr_reader :name, :species 
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @species = 'human'
+    @@all << self
   end
   
   def name
@@ -16,6 +19,18 @@ class Owner
   end
   
   def say_species
-    "I am a #{@species}"
+    "I am a #{@species}."
   end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count 
+    self.all.length 
+  end 
+  
+  def self.reset_all
+    self.all.clear 
+  end
 end
