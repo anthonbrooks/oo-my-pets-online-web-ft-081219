590dbfe465fa74061b6d03ce499fa9f68da77f20
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-31 16:07:31 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,10 +3,16 @@ class Cat
   attr_accessor :mood, :owner
   attr_reader :name 
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name 
     @owner = owner
     @mood = 'nervous'
+    @@all << self 
   end 
   
+  def self.all 
+    @@all 
+  end 
 end
