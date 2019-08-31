a54338ea3b6b6c3cdef40ab4cc0b2914d3bbfc6c
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-31 15:56:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :name 
+  attr_reader :species 
+  
+  def initialize(name, species)
+    @name = name 
+    @species = 'human'
+  end
+  
+  def name
+    @name 
+  end
+  
+  def species
+    @species
+  end
 end
